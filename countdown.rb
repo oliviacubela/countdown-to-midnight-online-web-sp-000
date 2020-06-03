4a5c0dfefa841b51bd2df85cb414f9b7abcc65f1
Olivia Cubela <oliviacubela@gmail.com>
2020-06-03 17:22:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
-#write your code here
+seconds_remaining = 10
+
+while seconds_remaining == 10
+  puts "#{seconds_remaining} SECOND(S)!"
+  seconds_remaining -= 1
+end#write your code here
 
 def countdown
 
